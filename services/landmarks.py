92f62a96c88c94ab9022141ae5ca4f92048195c2
zefparis <ben.barere@gmail.com>
2026-04-20 05:38:16 +0200

    fix: comprehensive GPU/GL env vars for headless mediapipe (GALLIUM_DRIVER, EGL_PLATFORM)

--- services/landmarks.py
+++ services/landmarks.py
@@ -1,6 +1,10 @@
 import os
 os.environ['MEDIAPIPE_DISABLE_GPU'] = '1'
+os.environ['LIBGL_ALWAYS_SOFTWARE'] = '1'
+os.environ['GALLIUM_DRIVER'] = 'softpipe'
+os.environ['MESA_GL_VERSION_OVERRIDE'] = '3.3'
 os.environ['OPENCV_VIDEOIO_PRIORITY_MSMF'] = '0'
+os.environ['EGL_PLATFORM'] = 'surfaceless'
 
 import base64
 import urllib.request
